db02889610c258937c6e81f9fb58d2d026c2e3cb
jsr25 <ramosjuans@live.com.ar>
2021-09-08 21:30:33 -0500

    modification in SecurityConfiguration

--- src/main/java/org/ada/school/config/SecurityConfiguration.java
+++ src/main/java/org/ada/school/config/SecurityConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
+@EnableWebSecurity
+@EnableGlobalMethodSecurity( securedEnabled = true, jsr250Enabled = true, prePostEnabled = true )
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure( HttpSecurity http )

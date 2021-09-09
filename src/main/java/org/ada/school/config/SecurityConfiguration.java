a2c318e631315e41298155232b8b9ef85ee73158
jsr25 <ramosjuans@live.com.ar>
2021-09-08 20:23:59 -0500

    The SecurityConfiguration class was created

---
+++ src/main/java/org/ada/school/config/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package org.ada.school.config;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+    @Override
+    protected void configure( HttpSecurity http )
+            throws Exception
+    {
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers( HttpMethod.GET, "/v1/health" ).permitAll()
+                .antMatchers( HttpMethod.POST,"/v1/auth" ).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS );
+    }
+
+}
+
